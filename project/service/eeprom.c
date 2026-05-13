4826981bb4f9ffe88fa2a14130ff9555294b5b1b
yezi <2976338413@qq.com>
2026-05-13 17:56:38 +0800

    完赛2

--- project/service/eeprom.c
+++ project/service/eeprom.c
@@ -28,22 +28,22 @@ static void eeprom_load_defaults(AppConfig *config)
     config->start.start_flag = 1;             /* 默认启动 */
     config->start.circle_flags = 1;           /* 默认关闭圆环识别 */
     config->start.track_mode = 0;             /* 默认左圆环->圆筒循环 */
-    config->start.fuya_xili = 55.00f;         /* 默认平地负压百分比 */
+    config->start.fuya_xili = 55.00f;         /* 默认平地负压百分比 70*/
     config->start.fuya_wall_percent = 60.00f; /* 默认墙面负压百分比 */
 
     /* 速度环 PID 默认参数 */
-    config->speed.kp_Err = 3.00f;  // 4.50
-    config->speed.kd_Err = 4.00f; // 4.50
+    config->speed.kp_Err = 3.00f;  // 3.50
+    config->speed.kd_Err = 4.00f; // 7.00
     config->speed.gyro_damp_Err = 0.00f;
-    config->speed.speed_run = 40.00f; /* 默认基础速度 30 */ // 4.50
-    config->speed.limiting_Err = 600.00f; /* 转向限幅 */    // 4.50
+    config->speed.speed_run = 40.00f; /* 默认基础速度 50 */ 
+    config->speed.limiting_Err = 600.00f; /* 转向限幅 */  
     config->speed.kp2_Err = 0.00f;
 
     /* 电感偏差解算默认参数 */
-    config->angle.kp_Angle = 0.80f;
-    config->angle.kd_Angle = 0.20f;
+    config->angle.kp_Angle = 0.80f;//0.85
+    config->angle.kd_Angle = 0.20f;//0.4
     config->angle.gyro_feedback_scale = 1.00f;
-    config->angle.limiting_Angle = 38.00f;
+    config->angle.limiting_Angle = 38.00f;//48
     config->angle.A_1 = 1.00f;
     config->angle.B_1 = 1.20f;
     config->angle.C_l = 0.60f;
@@ -52,9 +52,9 @@ static void eeprom_load_defaults(AppConfig *config)
     config->ring.ring_entry_encoder = 7.0;        /* ring->pre_ring编码器积分阈值 */
     config->ring.pre_ring_Gyro_target = 15.00f;     /* pre_ring固定目标角速度 */
     config->ring.pre_ring_Gyroz = 20.00f;           /* pre_ring->in_ring累计转角阈值 */
-    config->ring.in_ring_Gyroz = 210.00f;           /* in_ring->pre_out_ring累计转角阈值 */
-    config->ring.pre_out_ring_Gyro_target = 5.00f; /* pre_out_ring固定目标角速度 */
-    config->ring.pre_out_ring_Gyroz = 220.00f;      /* pre_out_ring->out_ring累计转角阈值 */
+    config->ring.in_ring_Gyroz = 210.00f;           /* in_ring->pre_out_ring累计转角阈值150 */
+    config->ring.pre_out_ring_Gyro_target = 5.00f; /* pre_out_ring固定目标角速度 15*/
+    config->ring.pre_out_ring_Gyroz = 220.00f;      /* pre_out_ring->out_ring累计转角阈值160 */
 
     /* 飞坡策略默认参数 */
     config->fly.count_fly_speed = 23;  /* 飞坡慢速值 */
--- project/service/motor.c
+++ project/service/motor.c
@@ -300,7 +300,7 @@ void dianya_jiance(void)
     }
 
     /* 持续欠压 3000 次（软件滤波，防止启动大电流导致电压跌落误判） */
-    if (dianya_count > 1000)
+    if (dianya_count > 3000)
     {
         stop = 1; /* 锁定停车，保护电池 */
     }
