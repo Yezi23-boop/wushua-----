0a90b2b7295215e2581ff263651e5915460135b0
yezi <2976338413@qq.com>
2026-05-24 19:11:03 +0800

    更换了主板

--- project/user/main.c
+++ project/user/main.c
@@ -44,10 +44,10 @@ void main()
                 //			printf("%f,%f,%f,%f\n", PID.left_speed.speed, PID.right_speed.speed, P46, speed_r);
                 //			ips114_show_int32(4 * 24, 18 * 0,P36, 4);
                 ////			P43=0;
-                ips114_show_float(4 * 24, 18 * 0, PID.left_speed.speed, 4, 1);
-                ips114_show_float(4 * 24, 18 * 1, PID.right_speed.speed, 4, 1);
+//                ips114_show_float(4 * 24, 18 * 0, PID.left_speed.speed, 4, 1);
+//                ips114_show_float(4 * 24, 18 * 1, PID.right_speed.speed, 4, 1);
 //			system_delay_ms(200);
-//          printf_imu();
+          printf_imu();
 //			printf_adc();
 //			printf_speed_test();
 #endif
